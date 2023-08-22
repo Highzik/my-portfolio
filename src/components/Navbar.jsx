b63dd8958f589b65d7ffc8bac7ab7afb39ccf966
Highzik <ajadiisaac99@gmail.com>
2023-08-22 11:19:17 +0100

    installed react-icons, bootstrap, and an icon for the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FaTimes } from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='container'>Navbar <FaTimes />
+      </div>
+
+    </>
+  )
+}
+
+export default Navbar
